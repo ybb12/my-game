1b0221f55ad07f34032736940be63dfd2af804c6
TheBusinessYuval <83600125+ybb12@users.noreply.github.com>
2025-06-19 08:28:51 +0300

    added basic movement

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,25 @@ use macroquad::prelude::*;
 #[macroquad::main("My game")]
 async fn main() {
     loop {
-        clear_background(DARKPURPLE);
+        let mut x = screen_width() /2.0 ;
+        let mut y = screen_height() /2.0 ;
+
+        if is_key_down(KeyCode::Right) {
+            x += 1.0;
+        }
+        if is_key_down(KeyCode::Left) {
+            x -= 1.0;
+        }
+        if is_key_down(KeyCode::Down) {
+            y += 1.0;
+        }
+        if is_key_down(KeyCode::Up) {
+            y -= 1.0;
+        }
+
+        draw_circle(x, y, 16.0, YELLOW);
+
+        clear_background(DARKBLUE);
         next_frame().await
     }
 }
